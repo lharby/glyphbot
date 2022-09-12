910852825db73ae31aaa97e2acc86cfaad33f7af
Luke Harby <Luke.Harby@akqa.com>
2022-09-12 10:16:39 +0100

    Add error logging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
+import { Stream } from 'stream';
 
 import Twit from 'twit';
 import { rndAlphabet, rndFontFamily } from './components/arrays.js';
@@ -8,6 +9,11 @@ import { rndAlphabet, rndFontFamily } from './components/arrays.js';
 if (process.env.NODE_ENV !== 'production') {
     dotenv.config();
 }
+
+let today = new Date();
+today = today.toLocaleString('en-gb');
+const errorFile = path.join('src', 'log', 'errors.txt');
+const stream = fs.createWriteStream(errorFile, { flags: 'a' });
 
 const configTwitter = {
     consumer_key: process.env.NEXT_TWITTER_CONSUMER_KEY,
@@ -26,6 +32,8 @@ const readImageFile = () => {
     fs.readdir(path.join('src', 'img'), (err, files) => {
         if (err || files.length !== 1) {
             console.log(`There was an error reading the file: ${err}`);
+            stream.write(`${today}. Error reading from file: ${err} \n`);
+            stream.end();
         } else {
             processFiles(files[0]);
         }
@@ -46,6 +54,8 @@ const readImageFile = () => {
 const mediaUploaded = (err, data, response) => {
     if (err) {
         console.log(`There was a mediaUploaded error: ${err}`);
+        stream.write(`${today}. Error from mediaUploaded: ${err} \n`);
+        stream.end();
     } else {
         // Now we can reference the media and post a tweet
         // with the media attached
@@ -62,8 +72,10 @@ const mediaUploaded = (err, data, response) => {
 const postData = (err, data, response) => {
     if (err) {
         console.log(`There was an postData error: ${err}`);
+        stream.write(`${today}. Error from postData: ${err} \n`);
+        Stream.end();
     } else {
-        console.log(`success!: ${JSON.stringify(data)}`);
+        console.log(`success!: ${JSON.stringify(data.created_at)}`);
     }
 };
 
